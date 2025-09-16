706a1cdef1f436c193281b00ae824b1510645658
Diogo Nascimento <diogocoding@gmail.com>
2025-09-16 07:57:40 -0300

    16/09/2025 07:57

--- Program.cs
+++ Program.cs
@@ -1,7 +1,28 @@
 ﻿using Csharp_IntermediateLevel.Models;
 
-Pessoa p1 = new Pessoa();
+
+
+Pessoa p1 = new Pessoa(nome:"João", sobrenome:"Nacimento");
+
+Pessoa p2 = new Pessoa(nome:"Eduardo", sobrenome:"Buta");
+
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+/*Pessoa p1 = new Pessoa();
 p1.Nome = "João";
 p1.Sobrenome = "Nascimento";
 p1.Idade = 34;
-p1.Apresentar();
+p1.Apresentar();*/
