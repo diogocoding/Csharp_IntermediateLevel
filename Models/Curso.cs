b78e77ced75a1cf7cff5adfa9399577fb824029c
Diogo Nascimento <diogocoding@gmail.com>
2025-09-17 09:49:58 -0300

    17/09/2025 09:49

--- Models/Curso.cs
+++ Models/Curso.cs
@@ -30,10 +30,13 @@ namespace Csharp_IntermediateLevel.Models
         public void ListarAlunos()
         {
             Console.WriteLine($"Alunos do curso de: {Nome}");
-            foreach (Pessoa aluno in Alunos)
+
+            for (int count = 0; count < Alunos.Count; count++)
             {
-                Console.WriteLine(aluno.NomeCompleto);
+                string texto = $"N° {count + 1} -> {Alunos[count].NomeCompleto}";
+                Console.WriteLine(texto);
             }
+
         }
 
     }
